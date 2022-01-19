734bd2b56eb4b476b9bda2ddb2fbd134ff257978
Omar Chammaa <chammaa.omar@gmail.com>
2022-01-20 00:19:12 +0300

    build read functions and expose them in message store interface

--- video_tutorials/src/message-store/index.js
+++ video_tutorials/src/message-store/index.js
@@ -1,9 +1,17 @@
+const createRead = require('./read');
 const createWrite = require('./write');
+const configureCreateSubscription = require('./subscribe');
 
 function createMessageStore({ db }) {
+	const { read, readLastMessage } = createRead({ db });
 	const write = createWrite({ db });
+	const createSubscription = configureCreateSubscription({ read, readLastMessage, write });
+
 	return {
 		write,
+		createSubscription,
+		read,
+		readLastMessage,
 	};
 }
 
---
+++ video_tutorials/src/message-store/read.js
@@ -0,0 +1,25 @@
+const deserializeMssage = require('./deserialize-message');
+
+const getLastMessageSql = 'SELECT * FROM get_last_stream_message($1)';
+const getStreamMessagesSql = 'SELECT * FROM get_stream_messages($1, $2, $3)';
+const getCategoryMessagesSql = 'SELECT * FROM get_category_messages($1, $2, $3)';
+
+function createRead({ db }) {
+	function readLastMessage(streamName) {
+		return db.query(getLastMessageSql, [streamName])
+			.then(res => deserializeMssage(res.rows[0]));
+	}
+
+	function read(streamName, fromPosition = 0, maxMessages = 1000) {
+		const query = streamName.includes('-') ? getStreamMessagesSql : getCategoryMessagesSql;
+		return db.query(query, [streamName, fromPosition, maxMessages])
+			.then(res => res.rows.map(deserializeMssage));
+	}
+
+	return {
+		read,
+		readLastMessage,
+	};
+}
+
+module.exports = createRead;
