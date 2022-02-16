d433957ef3696e09fd1f5a976337f1310ef36aa7
Omar Chammaa <chammaa.omar@gmail.com>
2022-02-15 21:44:54 -0500

    add explanation for replaying events

--- video_tutorials/src/components/identity/load-register-state.js
+++ video_tutorials/src/components/identity/load-register-state.js
@@ -6,7 +6,11 @@ const identityProjection = {
 		isRegistered: false,
 	}),
 
-
+	// When handling the RegisterUser command, we replay all the events in the identity entity stream
+	// for this particular user. If we find a Registered event, we transition into the state
+	// isRegistered: true. This enables us to handle the RegisterUser commands idempotently.
+	// Other events we can use to transition the state machine into other states: UserDeactivated
+	// for when the user deactivates their account 
 	Registered: (state, event) => {
 		state.id = event.data.userId;
 		state.email = event.data.email;
